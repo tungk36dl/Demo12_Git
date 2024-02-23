745ae5aa8bbed558002053319678d572e8e38726
Pham The Tung <tungxeco55@gmail.com>
2024-02-23 19:51:25 +0700

    Hello o chu lan 2

--- src/com/tt/Program2.java
+++ src/com/tt/Program2.java
@@ -1,10 +1,12 @@
 package com.tt;
 
-public class Program {
+public class Program2 {
     public static void main(String[] args) {
         System.out.println("Hello Gitty!");
         System.out.println("Hello Git 2!");
         System.out.println("Hello Git 3!");
+        System.out.println("Hello Git 3!");
         System.out.println("Hello Git 4!");
     }
+
 }
